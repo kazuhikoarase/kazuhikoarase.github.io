03e0ca2595da37043360f37e0aba52150683ff9c
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2016-05-02 11:53:29 +0900

    commit

--- devchk/devchk.js
+++ devchk/devchk.js
@@ -44,32 +44,31 @@ $(function() {
 
     var _w = 0;
     var _h = 0;
-    var ac0 = { absolute: false, alpha: 0, beta: 0, gamma: 0 };
-    var ac1 = { x: 0, y: 0, z: 0 };
-    var ac2 = { x: 0, y: 0, z: 0 };
-
-    window.addEventListener('deviceorientation', function(event) {
-      ac0 = event;
-    });
-
-    window.addEventListener('devicemotion', function(event) {
-      if (event.acceleration && event.acceleration.x != null) {
-        ac1 = event.acceleration;
-        ac2 = event.accelerationIncludingGravity;
-      }
-    });
+    var _ac0 = { absolute: false, alpha: 0, beta: 0, gamma: 0 };
+    var _ac1 = { x: 0, y: 0, z: 0 };
+    var _ac2 = { x: 0, y: 0, z: 0 };
 
     function getTime() {
       return +new Date();
     }
+    var _lastTime = getTime();
 
-    var lastTime = getTime();
+    window.addEventListener('deviceorientation', function(event) {
+      _ac0 = event;
+    });
+
+    window.addEventListener('devicemotion', function(event) {
+      if (event.acceleration && event.acceleration.x != null) {
+        _ac1 = event.acceleration;
+        _ac2 = event.accelerationIncludingGravity;
+      }
+    });
 
     function enterFrame(event) {
 
       time = getTime();
-      var dt = time - lastTime;
-      lastTime = time;
+      var dt = time - _lastTime;
+      _lastTime = time;
 
       var w = $(window).width();
       var h = $(window).height();
@@ -93,30 +92,30 @@ $(function() {
 
       var tbl = [
         ['window-size', _w + 'x' + _h],
-        ['do.absolute', ac0.absolute],
-        ['do.alpha', formatNumber(ac0.alpha)],
-        ['do.beta', formatNumber(ac0.beta)],
-        ['do.gamma', formatNumber(ac0.gamma)],
-        ['dm.x', formatNumber(ac1.x)],
-        ['dm.y', formatNumber(ac1.y)],
-        ['dm.z', formatNumber(ac1.z)],
-        ['dmIG.x', formatNumber(ac2.x)],
-        ['dmIG.y', formatNumber(ac2.y)],
-        ['dmIG.z', formatNumber(ac2.z)],
-        ['DeltaTime', formatNumber(dt)]
+        ['do.absolute', _ac0.absolute],
+        ['do.alpha', formatNumber(_ac0.alpha)],
+        ['do.beta', formatNumber(_ac0.beta)],
+        ['do.gamma', formatNumber(_ac0.gamma)],
+        ['dm.x', formatNumber(_ac1.x)],
+        ['dm.y', formatNumber(_ac1.y)],
+        ['dm.z', formatNumber(_ac1.z)],
+        ['dmIG.x', formatNumber(_ac2.x)],
+        ['dmIG.y', formatNumber(_ac2.y)],
+        ['dmIG.z', formatNumber(_ac2.z)],
+        ['dt', formatNumber(dt)]
       ];
 
       var $tbody = $('<tbody></tbody>');
       for (var i = 0; i < tbl.length; i += 1) {
         $tbody.append($('<tr></tr>').
-            append($('<th></th>').text(tbl[i][0] + ':') ).
-            append($('<td></td>').text('' + tbl[i][1]) ) );
+          append($('<th></th>').text(tbl[i][0] + ':') ).
+          append($('<td></td>').text('' + tbl[i][1]) ) );
       }
 
-      $info.append($('<br/>') );
-      $info.append($('<div></div>').addClass('box').
-          append($('<table></table>').
-          addClass('info-tbl').append($tbody) ) );
+      $info.append($('<br/>') ).
+        append($('<div></div>').addClass('box').
+        append($('<table></table>').
+        addClass('info-tbl').append($tbody) ) );
     }
 
     var $svg = createSVGElement('svg').css('position', 'absolute').
