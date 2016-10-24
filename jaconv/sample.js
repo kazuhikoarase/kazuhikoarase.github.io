8bd52d79149d9193f03149e55a78d575fad8030a
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2016-10-24 23:03:42 +0900

    commit

--- jaconv/sample.js
+++ jaconv/sample.js
@@ -1,31 +1,39 @@
 $(function() {
   
   var funcs = [
+    'toHebon',
+    'toKatakana',
+    'toHiragana',
     'toHanAscii',
     'toZenAscii',
     'toHanKana',
     'toZenKana',
     'toHan',
     'toZen',
-    'normalize',
-    'toKatakana',
-    'toHiragana',
-    'toHebon'
+    'normalize'
   ];
-
+  var $inTxt = $('<input type="text" />').addClass('txt').
+    attr('placeholder', 'ここに文字を入力').
+    on('keyup', function(event) {
+      $.each(funcs, function(i, func) {
+        $('#' + func).val(jaconv[func]($inTxt.val() ) );
+      } );
+    });
+  
+  var $body = $('#placeHolder').children('TBODY');
+  var addRow = function($cell1, $cell2) {
+    $body.append($('<tr></tr>').
+        append($('<td></td>').addClass('lbl').append($cell1) ).
+        append($('<td></td>').append($cell2) ) );
+  };
+  addRow($('<span></span>').text(''), $inTxt);
   $.each(funcs, function(i, func) {
-    $('#funcs').append($('<input type="radio"/>').
-        attr('id', func).attr('name', 'func').
-          prop('checked', i == funcs.length - 1).
-        on('click', function() { $('#t1').trigger('keyup'); }) ).
-        append($('<label></label>').
-        attr('for', func).text(func) ).
-        append($('<br/>'));
+    addRow($('<span></span>').text('jconv.' + func),
+        $('<input type="text" />').
+        addClass('txt').addClass('result').
+        attr('id', func).
+        attr('tabindex', '-1').
+        prop('readonly', true) );
   });
-  $('#t1').on('keyup', function() {
-    var func = $('input[name=func]:checked').attr('id');
-    $('#t2').val(jaconv[func]($('#t1').val() ) );
-  }).trigger('keyup');
-  $('#t2').prop('readonly', true).css('background-color', '#f0f0f0');
-
+  $inTxt.focus();
 });
