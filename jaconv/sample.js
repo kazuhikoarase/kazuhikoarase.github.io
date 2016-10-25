880cc54cae6947fea03501c0380cce9b1bf22317
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2016-10-25 11:23:11 +0900

    commit

--- jaconv/sample.js
+++ jaconv/sample.js
@@ -1,22 +1,22 @@
 $(function() {
   
   var funcs = [
-    'toHebon',
-    'toKatakana',
-    'toHiragana',
-    'toHanAscii',
-    'toZenAscii',
-    'toHanKana',
-    'toZenKana',
-    'toHan',
-    'toZen',
-    'normalize'
+    { name: 'toHebon', desc: '全角ひらがなをヘボン式で半角英文字に変換' },
+    { name: 'toKatakana', desc: '全角ひらがなを全角カタカナに変換' },
+    { name: 'toHiragana', desc: '全角カタカナを全角ひらがなに変換' },
+    { name: 'toHanAscii', desc: '全角英数記号を半角に変換' },
+    { name: 'toZenAscii', desc: '半角英数記号を全角に変換' },
+    { name: 'toHanKana', desc: '全角カタカナを半角に変換' },
+    { name: 'toZenKana', desc: '半角カタカナを全角に変換' },
+    { name: 'toHan', desc: '全角英数記号、カタカナを半角に変換' },
+    { name: 'toZen', desc: '半角英数記号、カタカナを全角に変換' },
+    { name: 'normalize', desc: '全角英数記号を半角に、半角カタカナを全角に変換' }
   ];
   var $inTxt = $('<input type="text" />').addClass('txt').
     attr('placeholder', 'ここに文字を入力').
     on('keyup', function(event) {
       $.each(funcs, function(i, func) {
-        $('#' + func).val(jaconv[func]($inTxt.val() ) );
+        $('#' + func.name).val(jaconv[func.name]($inTxt.val() ) );
       } );
     });
   
@@ -28,10 +28,11 @@ $(function() {
   };
   addRow($('<span></span>').text(''), $inTxt);
   $.each(funcs, function(i, func) {
-    addRow($('<span></span>').text('jconv.' + func),
+    addRow($('<span></span>').text('jconv.' + func.name).
+          attr('title', func.desc),
         $('<input type="text" />').
         addClass('txt').addClass('result').
-        attr('id', func).
+        attr('id', func.name).
         attr('tabindex', '-1').
         prop('readonly', true) );
   });
