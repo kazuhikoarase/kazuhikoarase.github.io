20a6b5a1fb4dcb18eb7b68972c3503e15d3bd117
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2017-02-06 15:49:24 +0900

    commit

---
+++ spherical-viewer/main.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+
+  var params = function(query_string) {
+    var params = {};
+    var kvList = query_string.split(/&/g);
+    for (var i = 0; i < kvList.length; i += 1) {
+      var kv = kvList[i].split(/=/);
+      if (kv.length == 2) {
+        params[kv[0]] = decodeURIComponent(kv[1]);
+        encodeURIComponent('/?&');
+      }
+    }
+    return params;
+  }(location.search? location.search.substring(1) : '');
+
+  var imageUrl = document.getElementById('imageUrl');
+  var getUrl = document.getElementById('imageUrl');
+  getUrl.addEventListener('click', function(event) {
+    if (imageUrl.value) {
+      location.href = '?url=' + encodeURIComponent(imageUrl.value);
+    }
+  });
+
+  var opts = {
+    src : params.src || 'my-picture.jpg',
+    maxTextureSize : 2048
+  };
+  var viewer = spherical_viewer(opts);
+  viewer.canvas.addEventListener('dblclick', function() {
+    viewer.toggleFullscreen();
+  });
+  document.body.appendChild(viewer.canvas);
+};
