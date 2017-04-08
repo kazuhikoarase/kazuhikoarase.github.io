130288635cc4bc4c3c6cf3c8637727fa33657dda
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2017-04-08 14:27:57 +0900

    commit

--- spherical-viewer/main.js
+++ spherical-viewer/main.js
@@ -3,6 +3,8 @@ window.onload = function() {
 
   'use strict';
 
+  var defaultSearch = 'sv_params=%7B"url"%3A"ueno_park.jpg"%2C"p"%3A6.144685416927639%2C"t"%3A0.02010945165123477%2C"z"%3A1.2313168871259323%7D';
+
   var params = function(query_string) {
     var params = {};
     var kvList = query_string.split(/&/g);
@@ -13,7 +15,7 @@ window.onload = function() {
       }
     }
     return params;
-  }(location.search? location.search.substring(1) : '');
+  }(location.search? location.search.substring(1) : defaultSearch);
 
   var dbltap = function(handler) {
     var getTime = function() { return new Date().getTime(); };
